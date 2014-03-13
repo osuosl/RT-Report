1b51c337d5945d63d8b1b53aa27793e1dde9dbff
Justin Dugger <jldugger@osuosl.org>
2014-03-13 15:07:46 -0700

    basic working script

---
+++ report.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import MySQLdb
+import config
+
+db = MySQLdb.connect(config.host,
+                     config.user,
+                      config.passwd,
+                      config.db)
+
+# you must create a Cursor object. It will let
+#  you execute all the query you need
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SELECT COUNT(id) FROM Tickets")
+
+# print all the first cell of all the rows
+for row in cur.fetchall() :
+    print row[0]
